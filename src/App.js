149a105fa0e810cee0b64e2ee1020d5ba1a60578
Chae <chae@aklas.kr>
2020-02-23 01:07:50 +0900

    .

--- src/App.js
+++ src/App.js
@@ -4,16 +4,92 @@ import Customer from './components/Customer'
 import './App.css';
 
 
-const customer = {
-    'name': '홍길동',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
-}
+const customers = [{
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '홍길동',
+        'birthday': '961222',
+        'gender': '남자',
+        'job': '대학생'
+    },
+    {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '나동빈',
+        'birthday': '960508',
+        'gender': '남자',
+        'job': '프로그래머'
+    }, {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '이순신',
+        'birthday': '961127',
+        'gender': '남자',
+        'job': '디자이너'
+    }
+]
 
 function App() {
-    return ( <
-        Customer / >
+    return ( < div >
+        <
+        Customer id = {
+            customers[0].id
+        }
+        image = {
+            customers[0].image
+        }
+        name = {
+            customers[0].name
+        }
+        birthday = {
+            customers[0].birthday
+        }
+        gender = {
+            customers[0].gender
+        }
+        job = {
+            customers[0].job
+        }
+        /> <
+        Customer id = {
+            customers[1].id
+        }
+        image = {
+            customers[1].image
+        }
+        name = {
+            customers[1].name
+        }
+        birthday = {
+            customers[1].birthday
+        }
+        gender = {
+            customers[1].gender
+        }
+        job = {
+            customers[1].job
+        }
+        /> <
+        Customer id = {
+            customers[2].id
+        }
+        image = {
+            customers[2].image
+        }
+        name = {
+            customers[2].name
+        }
+        birthday = {
+            customers[2].birthday
+        }
+        gender = {
+            customers[2].gender
+        }
+        job = {
+            customers[2].job
+        }
+        /> < /
+        div >
     );
 }
 
