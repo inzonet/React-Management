a3e4147a4090afb4971def37f988d8c73018b045
Chae <chae@aklas.kr>
2020-02-23 02:00:25 +0900

    .

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/api/customers', (req, res) => {
             'birthday': '961222',
             'gender': '남자',
             'job': '대학생'
-        },
+},
         {
             'id': 2,
             'image': 'https://placeimg.com/64/64/2',
@@ -24,16 +24,16 @@ app.get('/api/customers', (req, res) => {
             'birthday': '960508',
             'gender': '남자',
             'job': '프로그래머'
-        },
+},
         {
             'id': 3,
             'image': 'https://placeimg.com/64/64/3',
             'name': '이순신',
             'birthday': '961127',
             'gender': '남자',
-            'job': '디자이너'
-        }
-    ]);
+            'job': '디자이너스'
+}
+]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
