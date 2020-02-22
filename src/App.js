09f216b4265673389998ebf9cb5a7b1e7b6e6ab4
Chae <chae@aklas.kr>
2020-02-23 00:58:27 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+
+const customer = {
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+}
 
 function App() {
     return ( <
-        div className = "gray-background" >
-        <
-        img src = {
-            logo
-        }
-        alt = "logo" / >
-        <
-        h2 > Let 's develop management system!</h2> < /
-        div >
-
-
+        Customer / >
     );
 }
 
